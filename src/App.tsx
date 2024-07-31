35f34c486d4f60c7f54d1ab00616d436f771500d
KrisOP <krisortega15@hotmail.es>
2024-07-31 16:07:39 +0000

    agregando componente card

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Titulo from './Titulo.tsx'
+import Card from './components/Card.tsx'
 
 function App() {
   return  (
     <>
       <Titulo/>
+      <Card/>
+      
     </>
   )
 }
