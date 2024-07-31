608a3f7be627a58b38e0348763bb21525a2a8fc5
KrisOP <krisortega15@hotmail.es>
2024-07-31 17:51:16 +0000

    agregando listas, e iterando dentro de un componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import './App.css'
 import Titulo from './Titulo.tsx';
 //import Card from './components/Card.tsx';
-import Card, {CardBody} from './components/Card'
+import Card, {CardBody} from './components/Card';
+import List from './components/List.tsx';
 
 function App() {
+  const list = ['uno','dos','tres'];
   return  (
     <>
       <Titulo/>
       {/* <Card body={'Hola mundo'}/> */}
       <Card>
         <CardBody title={"Hola mundo xD"} text={"El texto del componente "}/>
+        <List data={list}/>
       </Card>
 
+   
 
       
     </>
