1c9dce50676d206804b80f348316365c0e516935
KrisOP <krisortega15@hotmail.es>
2024-07-31 15:33:42 +0000

    Iniciando proyecto e instalando bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Titulo from './Titulo.tsx'
+
+function App() {
+  return  (
+    <>
+      <Titulo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Titulo.tsx
@@ -0,0 +1,6 @@
+
+function Titulo(){
+    return <h1>Hola Mundo en componente nuevo</h1>
+}
+
+export default Titulo;
