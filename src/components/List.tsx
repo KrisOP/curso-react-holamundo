f5edaa8ad89f6888af8707c0c37bb092e90fef74
KrisOP <krisortega15@hotmail.es>
2024-08-02 19:41:53 +0000

    utilizando MouseEvent en onClick

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,20 @@
+import { MouseEvent } from "react";
+
 type Props = {
   data: string[];
 };
 
 function List({data}: Props) {
+
+  //leyendo el evento al hacer click a un objeto de la lista
+  const handleClick = (e:string)=>{
+    console.log(e);
+  }
   return (
     <ul className="list-group">
-        {data.map(element => <li key={element} className="list-group-item">{element}</li>)}
+        {/* {data.map(element => <li onClick={handleClick} key={element} className="list-group-item">{element}</li>)} */}
+
+        {data.map(element => <li onClick={()=>handleClick(element)} key={element} className="list-group-item">{element}</li>)}
     </ul>
   );
 }
