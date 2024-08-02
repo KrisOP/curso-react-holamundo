914474c265ae8eb9a1d845540d7970e0836d6982
KrisOP <krisortega15@hotmail.es>
2024-08-02 20:08:21 +0000

    gestion de estados usando useState

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,20 +1,33 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 type Props = {
   data: string[];
 };
 
-function List({data}: Props) {
-
+function List({ data }: Props) {
   //leyendo el evento al hacer click a un objeto de la lista
-  const handleClick = (e:string)=>{
-    console.log(e);
-  }
+
+  // const handleClick = (e:MouseEvent)=>{
+  //   console.log(e);
+  // }
+  const [index, setIndex] = useState(1);
+
+  const handleClick = (i: number) => {
+    setIndex(i);
+  };
   return (
     <ul className="list-group">
-        {/* {data.map(element => <li onClick={handleClick} key={element} className="list-group-item">{element}</li>)} */}
+      {/* {data.map(element => <li onClick={handleClick} key={element} className="list-group-item">{element}</li>)} */}
 
-        {data.map(element => <li onClick={()=>handleClick(element)} key={element} className="list-group-item">{element}</li>)}
+      {data.map((element, i) => (
+        <li
+          onClick={() => handleClick(i)}
+          key={element}
+          className={`list-group-item ${index == i ? "active" : ""}`}
+        >
+          {element}
+        </li>
+      ))}
     </ul>
   );
 }
