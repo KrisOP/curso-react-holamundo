d1cf4760896c93bc29de0abb604d54dcc4a5c24a
KrisOP <krisortega15@hotmail.es>
2024-08-05 16:47:53 +0000

    renderizado condicional

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,53 @@
-import './App.css'
-import Titulo from './Titulo.tsx';
+import "./App.css";
+import Titulo from "./Titulo.tsx";
 //import Card from './components/Card.tsx';
-import Card, {CardBody} from './components/Card';
-import List from './components/List.tsx';
+import Card, { CardBody } from "./components/Card";
+import List from "./components/List.tsx";
 
 function App() {
-  const list = ['uno','dos','tres'];
-  const handleSelect = (element:string) =>{
+  //const list = ['uno','dos','tres'];
+  const list: string[] = [];
+  const handleSelect = (element: string) => {
+    console.log("Imprimiendo " + element);
+  };
 
-    console.log("Imprimiendo " + element)
-  }
+  const handleSelect2 = (element: string) => {
+    console.log("Mostrando " + element);
+  };
 
-  const handleSelect2 = (element:string) =>{
+  //si hay elementos en la lista mostrarlos
+  // const contenido = list.length ? (
+  //   <>
+  //   <List data={list} onSelect={handleSelect} />
+  //   <List data={list} onSelect={handleSelect2} /></>
+ 
+  // ) : (
+  //   "Sin elementos que mostrar"
+  // );
 
-    console.log("Mostrando " + element)
-  }
-  return  (
+  const contenido = list.length !== 0 ? (
     <>
-      <Titulo/>
+    <List data={list} onSelect={handleSelect} />
+    <List data={list} onSelect={handleSelect2} /></>
+ 
+  ) : (
+    "Sin elementos que mostrar"
+  );
+
+
+  return (
+    <>
+      <Titulo />
       {/* <Card body={'Hola mundo'}/> */}
       <Card>
-        <CardBody title={"Hola mundo xD"} text={"El texto del componente "}/>
-        <List data={list} onSelect={handleSelect}/>
+        <CardBody title={"Hola mundo xD"} text={"El texto del componente "} />
+        {/* <List data={list} onSelect={handleSelect} />
 
-        <List data={list} onSelect={handleSelect2}/>
-
+        <List data={list} onSelect={handleSelect2} /> */}
+        {contenido}
       </Card>
-
-   
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
