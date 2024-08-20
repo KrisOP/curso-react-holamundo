118e46334a77c5f7686ac4b616686bd7c33acce8
KrisOP <krisortega15@hotmail.es>
2024-08-20 17:47:24 +0000

    reestructurando carpetas - Curso React Platzi

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,22 +1,19 @@
 import "./App.css";
-import Titulo from "../Titulo.tsx";
+import Titulo from "../../Titulo.tsx";
 //import Card from './components/Card.tsx';
-import Card, { CardBody } from "../components/Card.tsx";
-import Button from "../components/Button/index.tsx";
-import List from "../components/List.tsx";
+import Card, { CardBody } from "../Card.tsx";
+import Button from "../Button/index.tsx";
+import List from "../List.tsx";
 import { useState } from "react";
-import { TodoItem } from "../components/TodoItem/index.tsx";
-import CreateTodoButton from "../components/CreateTodoButton/index.tsx";
-import { TodoCounter } from "../components/TodoCounter/index.tsx";
-import { TodoSearch } from "../components/TodoSearch/index.tsx";
-import { TodoList } from "../components/TodoList/index.tsx";
+import { TodoItem } from "../TodoItem/index.tsx";
+import CreateTodoButton from "../CreateTodoButton/index.tsx";
+import { TodoCounter } from "../TodoCounter/index.tsx";
+import { TodoSearch } from "../TodoSearch/index.tsx";
+import { TodoList } from "../TodoList/index.tsx";
 import React from "react";
 import { useLocalStorage } from "./useLocalStorage.ts";
 
-import { DefaultTodosModel } from "../shared/model/defaultTodos.model.ts";
-
-
-
+import { DefaultTodosModel } from "../../shared/model/defaultTodos.model.ts";
 
 function App() {
   const [Data, setData] = useState(["uno", "dos", "tres"]);
@@ -80,8 +77,6 @@ function App() {
 
   //defaultTodosModel defaultTodos[] = new defaultTodosModel();
 
- 
-
   let firstDefaultItems = [
     { text: "Cortar cebolla", completed: true },
     { text: "Tomar el cursso de intro a React", completed: false },
@@ -89,8 +84,7 @@ function App() {
     { text: "Nuevo", completed: true },
   ];
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', firstDefaultItems);
-
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", firstDefaultItems);
 
   const completedTodos = todos.filter(
     (todo: DefaultTodosModel) => !!todo.completed
@@ -102,8 +96,6 @@ function App() {
     const searchText = searchValue.toLowerCase();
     return todoText.includes(searchText);
   });
-
-
 
   const completeTodo = (text: string) => {
     const newTodos = [...todos];
